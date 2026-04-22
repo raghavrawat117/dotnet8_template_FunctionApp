9e25393517d2ef3411ae30a16a6918660342347f
raghavrawat117 <raghav.rawat@capgemini.com>
2026-04-23 02:47:29 +0530

    Change Run method to async in HttpTriggeredFunction
    Updated Run method to be asynchronous.

--- SampleFunctionApp/HttpTriggeredFunction.cs
+++ SampleFunctionApp/HttpTriggeredFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace SampleFunctionApp
 {
@@ -15,7 +16,7 @@ namespace SampleFunctionApp
         }
 
         [Function("HttpTriggeredFunction")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
